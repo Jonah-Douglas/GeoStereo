928d6c4fdf9f4cf6e0db74a805d446779d68c626
Jonah Douglas <jonah.douglas23@gmail.com>
2022-06-06 17:47:48 -0700

    Initial commit failed, attempting fix after running config on user.email and user.name

---
+++ app/src/main/java/com/campfire/geostereo/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.campfire.geostereo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}

6144f8bb9a9e400b3f8e0681671b1a9f402ef216
Jonah Douglas <jonah.douglas23@gmail.com>
2022-06-13 14:55:44 -0700

    Established GeoStereo theme, implemented navigation, a toolbar with a menu, and created initial fragment. Next focus is querying Google Maps API via coroutine and storing the response for use.

--- app/src/main/java/com/campfire/geostereo/MainActivity.kt
+++ app/src/main/java/com/campfire/geostereo/MainActivity.kt
@@ -2,11 +2,57 @@ package com.campfire.geostereo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.campfire.geostereo.databinding.ActivityMainBinding
 
+// TODO: -create user profile
+//       -host user image
+//       -authenticate via Facebook
+//       -implement messaging between users
 
+/**
+ * Main activity that hosts all fragments for GeoStereo.
+ */
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+
+        val navController = findNavController(R.id.nav_host_fragment_content_main)
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        // Inflate the menu -> adds item menus to action bar if present
+        menuInflater.inflate(R.menu.menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment_content_main)
+        return navController.navigateUp(appBarConfiguration)
+                ||super.onSupportNavigateUp()
     }
 }
---
+++ app/src/main/java/com/campfire/geostereo/ui/FindNearestLocationFragment.kt
@@ -0,0 +1,40 @@
+package com.campfire.geostereo.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import com.campfire.geostereo.databinding.FragmentFindNearestLocationBinding
+
+
+class FindNearestLocationFragment : Fragment() {
+
+    private var _binding: FragmentFindNearestLocationBinding? = null
+    // Property only valid between onCreateView and onDestroyView, so !! ok
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentFindNearestLocationBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.findNearestLocation.setOnClickListener {
+            // TODO: Google Maps search here (create coroutine call to do searches so it's nonblocking
+            // TODO: On Result, navigate to new fragment
+                // shows nearest location
+                // if nothing nearby, query a weather API
+            // TODO: Use final result to query Spotify API (again coroutine call)
+                // show song playing in app, have some features such as pause song, skip, etc.
+                // if I can't implement spotify in my app, use implicit/explicit Intent to pull up Spotify playlist on their phone and begin playing it
+            Toast.makeText(context, "Implement Google Maps search here, navigate to new fragment showing nearest location and song playing.", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
